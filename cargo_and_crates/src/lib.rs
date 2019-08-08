1736dbd482a18839fc7db6149c1e438fc857fea3
shyan1 <all_bright@live.com>
2019-08-08 14:05:45 +0800

    add example.rs for bench

--- cargo_and_crates/src/lib.rs
+++ cargo_and_crates/src/lib.rs
@@ -1,31 +1,31 @@
-#![feature(test)]
-extern crate test;
+// #![feature(test)]
+// extern crate test;
 
-pub fn add(x: i32, y: i32) -> i32 {
-    x + y
-}
+// pub fn add(x: i32, y: i32) -> i32 {
+//     x + y
+// }
 
-#[cfg(test)]
-mod tests {
-    use super::add;
-    use test::Bencher;
+// #[cfg(test)]
+// mod tests {
+//     use super::add;
+//     use test::Bencher;
 
-    use std::thread;
-    use std::time::Duration;
-    #[test]
-    fn this_works() {
-        assert_eq!(add(1, 2), 3);
-    }
+//     use std::thread;
+//     use std::time::Duration;
+//     #[test]
+//     fn this_works() {
+//         assert_eq!(add(1, 2), 3);
+//     }
 
-    #[test]
-    #[should_panic(expected = "attempt to add with overflow")]
-    fn this_does_not_work() {
-        assert_eq!(add(std::i32::MAX, std::i32::MAX), 0);
-    }
+//     #[test]
+//     #[should_panic(expected = "attempt to add with overflow")]
+//     fn this_does_not_work() {
+//         assert_eq!(add(std::i32::MAX, std::i32::MAX), 0);
+//     }
 
-    #[bench]
-    fn how_fast(b: &mut Bencher) {
-        b.iter(|| add(42, 42));
-        thread::sleep(Duration::from_millis(5000));
-    }
-}
+//     #[bench]
+//     fn how_fast(b: &mut Bencher) {
+//         b.iter(|| add(42, 42));
+//         thread::sleep(Duration::from_millis(5000));
+//     }
+// }
